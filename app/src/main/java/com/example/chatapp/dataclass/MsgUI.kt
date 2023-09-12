c1dcbc9055ad1c45fb5e8b1533d536d4a1a4e673
Prashanth G <45532271+Prashanth10@users.noreply.github.com>
2023-09-12 10:15:04 +0530

    Edit message added (#1)
    Co-authored-by: Prashanth G <prashanth.g@soti.net>

--- app/src/main/java/com/example/chatapp/dataclass/MsgUI.kt
+++ app/src/main/java/com/example/chatapp/dataclass/MsgUI.kt
@@ -1,6 +1,13 @@
 package com.example.chatapp.dataclass
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 
-data class MsgUI(val alignment: Alignment.Horizontal, val cardColor: Color, val userNameColor: Color, val msgColor: Color)
+data class MsgUI(
+    val arrangement: Arrangement.Horizontal,
+    val alignment: Alignment.Horizontal,
+    val cardColor: Color,
+    val userNameColor: Color,
+    val msgColor: Color
+)
