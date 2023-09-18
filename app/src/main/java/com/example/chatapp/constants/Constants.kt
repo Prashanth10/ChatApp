07a2654f98c371eb581092343196c668fc924015
Prashanth G <prashanth.g@soti.net>
2023-09-18 17:29:10 +0530

    chat messages

--- app/src/main/java/com/example/chatapp/constants/Constants.kt
+++ app/src/main/java/com/example/chatapp/constants/Constants.kt
@@ -8,4 +8,9 @@ object Constants {
     const val KEY_IS_SIGNED_IN = "isSignedIn"
     const val KEY_USER_ID = "userId"
     const val KEY_FCM_TOKEN = "fcmToken"
+    const val KEY_COLLECTION_CHAT = "chat"
+    const val KEY_SENDER_ID = "senderId"
+    const val KEY_RECEIVER_ID = "receiverId"
+    const val KEY_MESSAGE = "message"
+    const val KEY_TIMESTAMP = "timestamp"
 }
